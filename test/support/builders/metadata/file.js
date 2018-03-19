f01154745413a226ae64f7d2574ddf7b4ed516a4
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-03-19 18:04:34 +0100

    test/support/builders/metadata: Add more options

--- test/support/builders/metadata/file.js
+++ test/support/builders/metadata/file.js
@@ -4,13 +4,32 @@ import type { Metadata } from '../../../../core/metadata'
 
 const BaseMetadataBuilder = require('./base')
 const { assignId } = require('../../../../core/metadata')
+const Pouch = require('../../../../core/pouch')
 
 const pouchdbBuilders = require('../pouchdb')
 
 module.exports = class FileMetadataBuilder extends BaseMetadataBuilder {
+  fileOpts: {
+    size: number,
+    md5sum: string
+  }
+
+  constructor (pouch: ?Pouch) {
+    super(pouch)
+    this.fileOpts = {
+      size: 0,
+      md5sum: ''
+    }
+  }
+
+  data (data: string): this {
+    this.fileOpts.size = Buffer.from(data).length
+    // TODO: md5sum
+    return this
+  }
+
   build (): Metadata {
     const doc = {
-      ...this.opts,
       _id: '',
       // _rev: pouchdbBuilders.rev(),
       docType: 'file',
@@ -19,7 +38,9 @@ module.exports = class FileMetadataBuilder extends BaseMetadataBuilder {
         _rev: pouchdbBuilders.rev()
       },
       tags: [],
-      updated_at: new Date()
+      updated_at: new Date(),
+      ...this.opts,
+      ...this.fileOpts
     }
     assignId(doc)
     return doc
